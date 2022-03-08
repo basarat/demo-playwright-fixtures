d8c02a5e7086aba9c589cbd21217ff6af589b1dd
basarat <basaratali@gmail.com>
2022-03-08 13:25:31 +1100

    wrap

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,6 +38,12 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /** Only take screenshot on failure */
+    screenshot: 'only-on-failure',
+
+    /** Record video, but only keep it in case of failure */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
