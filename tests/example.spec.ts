07546516ea5b1dd649fd5fb77e7cf27c7c6ded35
basarat <basaratali@gmail.com>
2022-03-09 17:00:44 +1100

    wrap

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,25 +1,9 @@
-import { test, expect } from '@playwright/test';
-import { TodoPage } from './TodoPage';
+import { test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await new TodoPage(page).goto();
+test('demo fixtures', async ({ page }) => {
+
 });
 
-test('should allow me to add todo items', async ({ page }) => {
-  const todoPage = new TodoPage(page);
+test('demo reuse', async ({ page }) => {
 
-  // Create a todo
-  await todoPage.addTodo('buy some cheese');
-
-  // Make sure the list has the todo item
-  await todoPage.expectTodos(['buy some cheese']);
-
-  // Create 2nd todo.
-  await todoPage.addTodo('feed the cat');
-
-  // Make sure the list now has two todo items.
-  await todoPage.expectTodos([
-    'buy some cheese',
-    'feed the cat',
-  ]);
 });
